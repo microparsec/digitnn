811587ba87d9df3d6d5fe526797a6723d289a9bf
Casper Bezemer <bzmr@psg.nl>
2018-04-28 18:33:05 +0200

    Revision 1.1 - Cross entropy and more training features

--- README.md
+++ README.md
@@ -1,9 +1,20 @@
 # digitNN
 Simple neural network classifier on the MNIST digit set.
 
-The neural network is a backpropagating sigmoid activated multimatrix with double variable sized hidden nodes layer that achieves ~94% accuracy on default settings.
+The neural network is a backpropagating softmax activated multimatrix with double variable sized hidden nodes layer that achieves 96% accuracy on default settings.
 
-Comes with automatic training and easy variable adjustment for length and complexity of the training process.
+Comes with automatic training featuring epochs, mini-batches, automatic alpha adjustment and cutoff, validation error visualisation and hyperparamater configuration for many aspects of the training process.
+
+### Version: 1.0
+- Randomized mini-batch learning classifier by sigmoid activation
+- Sampler for user image prediction
+
+### Version 1.1
+- Cross-entropy loss and softmax activation
+- Epoch based learning, automatic training data shuffling, deterministic batching
+- Automatic alpha decay
+- Validation error visualiser, see which samples the network got wrong
+- Average 96% accuracy after three minutes of training
 
 ## Resources
 [Download the MNIST set yourself here](http://yann.lecun.com/exdb/mnist/)
--- nn.py
+++ nn.py
@@ -28,15 +28,17 @@ enable_sampler = False
 
 # network
 input_size = imagelength
-hidden_size = 50
+hidden_size = 100
 output_size = len(labels)
 
 # Training variables
-batchsize = 10          # amount of samples to process each iteration
-iterations = 10000
-alpha = 0.1             # amount of learning from one iteration
+batchsize = 50          # amount of samples to process each iteration
+epochs = 100            # amount of times to look through the data set
+alpha = 0.05            # amount of learning from one iteration
+alphadecay = .8         # alpha decay factor when loss increases
+alphacutoff = 0.0001    # stop converging if alpha drops below this threshold
 
-softloss = 0.001        # smoothing factor for loss indication
+softloss = 0.01         # smoothing factor for loss indication
 outputinterval = 23     # iterations per terminal update
 
 # TRAINING DATA SETUP
@@ -92,12 +94,21 @@ def sigmoid(s):
 def dsigmoid(s):
     return s * (1 - s)
 
+def softmax(s):
+    exps = np.exp(s - np.max(s, axis=-1,keepdims=True))
+    return exps / np.sum(exps, axis=-1,keepdims=True)
+
+def crossentropy(y_, y, e = 1e-12):
+    return -np.sum(y_ * np.log(y + e)) 
+
 def imagetostring(image):
     output = ""
     for y in range(0,imagesize):
         line = ""
         for x in range(0,imagesize):
-            if image[imagesize * y + x] > 0.5: line += "#"
+            if image[imagesize * y + x] > 0.8: line += "#"
+            elif image[imagesize * y + x] > 0.5: line += "$"
+            elif image[imagesize * y + x] > 0.3: line += ";"
             else: line += " "
         output += line + "\n"
     return output
@@ -106,12 +117,14 @@ def batchtostring(batch):
     string = ""
     for y in batch:
         n = np.argmax(y)
-        string += str(n) +" " + str(np.round(y, 2)) + "\n"
+        string += "%s %s\n" % (labels[n], str(np.round(y, 2)))
     return string
 
 
 # Simple neural network with two hidden layers
 class NeuralNetwork:
+
+    
 
     def __init__(self, N = 2, H = 3, O = 1):
         self.N = N
@@ -119,42 +132,51 @@ class NeuralNetwork:
         self.O = O
 
         self.W1 = np.random.randn(N, H)
+        self.B1 = np.zeros(H)
         self.W2 = np.random.randn(H, H)
+        self.B2 = np.zeros(H)
         self.W3 = np.random.randn(H, O)
+        self.B3 = np.zeros(O)
         
     def forward(self, X):
-        self.z = np.dot(X, self.W1)
+        self.z  = self.B1 + np.dot(X, self.W1)
         self.z2 = sigmoid(self.z)
-        self.z3 = np.dot(self.z2, self.W2)
+        self.z3 = self.B2 + np.dot(self.z2, self.W2)
         self.z4 = sigmoid(self.z3)
-        self.z5 = np.dot(self.z4, self.W3)
-
-        return sigmoid(self.z5)
+        self.z5 = self.B3 + np.dot(self.z4, self.W3)
+        return softmax(self.z5)
 
     def backward(self, X, y, o):
-        self.o_error = y - o
-        self.o_derror = self.o_error * dsigmoid(o)
+        #self.o_error = y - o
+        self.o_derror = o - y
 
-        self.z4_error = self.o_error.dot(self.W3.T)
+        self.z4_error = self.o_derror.dot(self.W3.T)
         self.z4_derror = self.z4_error * dsigmoid(self.z4)
 
         self.z2_error = self.z4_derror.dot(self.W2.T)
         self.z2_derror = self.z2_error * dsigmoid(self.z2)
 
         dW1 = X.T.dot(self.z2_derror)
+        dB1 = np.sum(self.z2_derror, axis=0)
         dW2 = self.z2.T.dot(self.z4_derror)
+        dB2 = np.sum(self.z4_derror, axis=0)
         dW3 = self.z4.T.dot(self.o_derror)
-        return dW1, dW2, dW3
+        dB3 = np.sum(self.o_derror, axis=0)
+        return dW1, dB1, dW2, dB2, dW3, dB3
 
 
-    def train(self, X, y, alpha = 1):
+    def train(self, X, y, batchsize, alpha = 1):
         o = self.forward(X)
-        dW1, dW2, dW3 = self.backward(X, y, o)
+        dW1, dB1, dW2, dB2, dW3, dB3 = self.backward(X, y, o)
 
-        self.W1 += alpha * dW1
-        self.W2 += alpha * dW2
-        self.W3 += alpha * dW3
+        self.W1 += -alpha * dW1
+        self.B1 += -alpha * dB1
+        self.W2 += -alpha * dW2
+        self.B2 += -alpha * dB2
+        self.W3 += -alpha * dW3
+        self.B3 += -alpha * dB3
 
+        return o
 
 # Start the program
 
@@ -162,38 +184,65 @@ NN = NeuralNetwork(imagelength, hidden_size, len(labels))
 
 # calculate starting loss value
 forward = NN.forward(X[0:batchsize])
-loss = np.mean(np.square(y[0:batchsize] - forward))
+loss = crossentropy(y[0:batchsize],forward) / batchsize
 
 starttime = time.time()
 
 # TRAIN
-for i in range(0, iterations):
+error = False
+for e in range(0, epochs):
 
-    batchstart = np.random.randint(0, trainingdatalength - batchsize)
-    batchend = batchstart + batchsize
 
-    forward = NN.forward(X[batchstart:batchend])
-    loss = np.mean(np.square(y[batchstart:batchend] - forward)) * softloss + (1-softloss) * loss
+    try:
+        epoch_startloss = loss
+        print('\rEpoch %i Loss: %.5f Alpha: %.5f ... ' % (e, loss, alpha), end="", flush = True)
 
-    if i % outputinterval == 0 or i == iterations - 1:
-        print('\rIteration %i Loss: %.5f ...' % (i + 1, loss), end="", flush = True)
+        # shuffle the dataset
+        indices = np.arange(X.shape[0])
+        np.random.shuffle(indices)
 
-    # alpha transition
-    # if i % 100000 == 0:
-    #     alpha /= 2
+        X = X[indices]
+        y = y[indices]
 
-    NN.train(X[batchstart:batchend], y[batchstart:batchend], alpha)
-print(" complete")
-print("Trained batches of %i iterations on %i samples in %.2f seconds \n" % (batchsize, iterations, time.time() - starttime))
+        for i in range(0, int(trainingdatalength / batchsize)):
+            batchstart = i * batchsize
+            batchend = (i + 1) * batchsize
+
+            forward = NN.train(X[batchstart:batchend], y[batchstart:batchend], batchsize, alpha)
+            loss = crossentropy(y[batchstart:batchend], forward) / batchsize * softloss + (1-softloss) * loss
+
+        #alpha transition
+        if loss > epoch_startloss:
+            alpha *= alphadecay
+            if alpha <= alphacutoff:
+                break
+
+    except KeyboardInterrupt:
+        error = True
+        print(' aborted')
+        break
+
+
+if not error:
+    print(" complete")
+print("Trained %i epochs on %i samples in %.2f seconds \n" % (e + 1, trainingdatalength, time.time() - starttime))
 
 
 print("Validating...\r", end="", flush="True")
 errors = 0
+show = True
 for i in range(0,validationdatalength):
-    prediction = NN.forward(vX[i])
+    prediction = NN.forward(vX[i:i+1])
 
     #       predicted               actual
     if np.argmax(prediction) != np.argmax(vy[i]):
+        if show and not error:
+            print(imagetostring(vX[i]))
+            print("Actual: %s, Prediction: %s" % (labels[np.argmax(vy[i])], batchtostring(prediction)))
+            print("Press 'q' to skip validation errors")
+            if input() == "q":
+                show = False
+
         errors += 1
 
 print("Validation: %i out of %i - %.2f%%" % (validationdatalength - errors, validationdatalength, (100.0 * (validationdatalength - errors) / validationdatalength)) )
@@ -201,7 +250,7 @@ print("Validation: %i out of %i - %.2f%%" % (validationdatalength - errors, vali
 # Sampler
 
 once = True
-while(enable_sampler or once):
+while((enable_sampler or once) and not error):
     once = False
 
     image = imageio.imread(sampleimage).reshape(imagesize, imagesize, 4)
